c4db4f01391d4bd52b47b17577ded453886300a0
Jean-Philippe Gelinas <jpgelinas@avencall.com>
2015-01-28 11:22:17 -0500

    enable graphviz plugin

--- source/conf.py
+++ source/conf.py
@@ -41,7 +41,8 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.todo', 'sphinx_git']
+extensions = ['sphinx.ext.todo', 'sphinx_git', 'sphinx.ext.graphviz']
+graphviz_output_format = 'svg'
 
 
 # Add any paths that contain templates here, relative to this directory.
