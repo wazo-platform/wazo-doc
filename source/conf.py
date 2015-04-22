1a05c5c4e5cc5e85be2e1bf0d76ba11a30bda6b6
Jean-Philippe Gelinas <jpgelinas@avencall.com>
2015-04-22 13:50:37 -0400

    change theme to default
    Goal is to have a mobile-friendly responsive design.

--- source/conf.py
+++ source/conf.py
@@ -114,7 +114,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = u'nature'
+html_theme = u'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
