8fb2c691edd252b9fc69636763c4fa4ddebaa24b
Francois Blackburn <fblackburn@wazo.community>
2017-12-11 13:12:13 -0500

    conf.py: fix comment
    this comment has been changed by the release script in version
    wazo-16.16 and bumped every iteration ...

--- source/conf.py
+++ source/conf.py
@@ -136,7 +136,7 @@ html_theme_options = {'logo_only': True}
 html_logo = '_static/img/logo.svg'
 
 # The name of an image file (within the static path) to use as favicon of the
-# docs.  This file should be a Windows icon file (.ico) being 18.01 or 32x32
+# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
 #html_favicon = None
 
